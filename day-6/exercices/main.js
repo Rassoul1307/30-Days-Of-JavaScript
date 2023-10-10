5e2e58bfdbd843bf4a9e71848022045e239cc069
rassoul fall <fallrassoul342@gmail.com>
2023-10-10 08:43:24 +0000

    day 6

---
+++ day-6/exercices/main.js
@@ -0,0 +1,61 @@
+// Niveau 1
+// 1 Itérer de 0 à 10 en utilisant la boucle for, faire de même en utilisant la boucle while et do while
+// ----------------For------------------------
+// for (let i = 0; i < 11; i++) {
+//     console.log(i);
+    
+// }
+// --------------While-----------------------
+// let nbr = 0
+// while (nbr <= 10) {
+//     console.log(nbr);
+//     nbr++
+// }
+
+// ------------Do while------------------------
+// let nbr = 0
+// do {
+//     console.log(nbr);
+//     nbr++;
+// } while (nbr<=10);
+
+// 2 Itérer de 10 à 0 en utilisant la boucle for, faire de même en utilisant la boucle while et do while
+
+// -------------------for---------------------------
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
+// --------------------while-------------------------
+// let nbr = 10
+// while (nbr >= 0) {
+//     console.log(nbr);
+//     nbr--;
+// }
+// ------------------do while-------------------------
+// let nbr=10
+// do {
+//     console.log(nbr);
+//     nbr--;
+// } while (nbr>=0);
+
+
+// 3  Itérer de 0 à n en utilisant la boucle for
+// let n = 5
+// for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+    
+// }
+
+// 4  Écrivez une boucle qui crée le modèle suivant en utilisant console.log() :
+// for (let index = 0; index < 7 ; index++) {
+//     console.log(`${"#".repeat(index)}`);
+// }
+
+// 5 Utilisez la boucle pour imprimer le motif suivant :
+// let nbr = 0
+// for (let index = 0; index <= 10; index++) {
+//     let calcule = `${nbr} * ${index}`
+//     let resultat = (nbr++) * index
+//     console.log(`${calcule} = ${resultat}`)
+    
+// }
