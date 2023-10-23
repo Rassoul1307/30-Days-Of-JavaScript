29891f7df409ab3172d75f491003109b94dcec21
rassoul fall <fallrassoul342@gmail.com>
2023-10-23 19:03:25 +0000

    day 19

---
+++ day-19/scripts/main.js
@@ -0,0 +1,41 @@
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+// Closure
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
+
+
+// Let us more example of inner functions
+function outerFunction() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = outerFunction()
+
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
